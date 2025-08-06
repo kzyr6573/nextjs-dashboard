a24d6016fafb8a01fa93e63c05c374eb3811976c
kzyr6573 <yy980767@dal.ca>
2025-08-06 20:08:30 -0300

    Modified dashboard to add ETH price widget

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <p>Dashboard Page</p>;
+  return <script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currencyid="1027" data-base="USD" data-secondary="" data-ticker="true" data-rank="false" data-marketcap="true" data-volume="true" data-statsticker="false" data-stats="USD"></div>;
 }
